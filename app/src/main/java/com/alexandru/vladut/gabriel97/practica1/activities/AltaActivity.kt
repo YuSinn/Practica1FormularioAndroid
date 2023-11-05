4b42e315c0298deac31530a28438068c6f18cc90
Vladut Gabriel Alexandru <alexandru.vladut.gabriel97@gmail.com>
2023-11-05 14:54:27 +0100

    Modificacion nr 3

--- app/src/main/java/com/alexandru/vladut/gabriel97/practica1/activities/AltaActivity.kt
+++ app/src/main/java/com/alexandru/vladut/gabriel97/practica1/activities/AltaActivity.kt
@@ -1,8 +1,11 @@
 package com.alexandru.vladut.gabriel97.practica1.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import com.alexandru.vladut.gabriel97.practica1.MainActivity
 import com.alexandru.vladut.gabriel97.practica1.R
 import com.alexandru.vladut.gabriel97.practica1.models.Usuario
 
@@ -23,5 +26,17 @@ class AltaActivity : AppCompatActivity() {
         textViewTipo.text = usuario.tipoUsuario
         textViewNombre.text = usuario.nombre
         textViewApellido.text = usuario.apellido
+
+        val botonVolver = findViewById<Button>(R.id.buttonVolver)
+
+        botonVolver.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Cerrar la actividad actual (AltaActivity)
+            finish()
+        }
+
+
     }
 }
